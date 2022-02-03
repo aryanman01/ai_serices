d00a26b75c817a399b940d98b24bcc3810fbe14b
Ankit Aryan <38923103+aryanman01@users.noreply.github.com>
2022-02-04 01:05:47 +0530

    Update etl.py

--- dryer/etl/etl.py
+++ dryer/etl/etl.py
@@ -1,2 +1,6 @@
-pylint: disable=missing-module-docstring
-print("hello world")
+import json
+
+with open('config/config1.json') as json_file:
+    data = json.load(json_file)
+
+print("hello world", data["env"])
